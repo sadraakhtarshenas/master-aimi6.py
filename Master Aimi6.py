ea35d35781719f5c7c26d012a5e2314a9aed9441
sadraakhtarshenas <96003970+sadraakhtarshenas@users.noreply.github.com>
2021-12-15 12:31:25 -0800

    Add files via upload

---
+++ Master Aimi6.py
@@ -0,0 +1,36 @@
+
+
+a = int(input())
+b = int(input())
+
+if b == 0:
+  if b != 0:
+    result = a / b
+
+print(result)
+
+
+a = int(input())
+b = int(input())
+
+if b == 0:
+    result = "khata!!!"
+
+if b != 0:
+    result = a / b
+
+print(result)
+
+
+
+a = int(input())
+b = int(input())
+
+if b == 0:
+    result = "khtata!!!"
+
+else:
+    result = a / b
+
+print(result)
+
